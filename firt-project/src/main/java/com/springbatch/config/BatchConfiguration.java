be282e17be37f1ab94aed7b1411f870286a04243
Marcelo Cartagena <marcelocartagenaorellana@gmail.com>
2026-09-20 17:04:06 -0500

    basic flow sequential

--- firt-project/src/main/java/com/springbatch/config/BatchConfiguration.java
+++ firt-project/src/main/java/com/springbatch/config/BatchConfiguration.java
@@ -13,7 +13,7 @@ import org.springframework.transaction.PlatformTransactionManager;
 @Configuration
 public class BatchConfiguration {
     @Bean
-    public Step firstStep(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+    public Step step1(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
         return new StepBuilder("step1", jobRepository)
                 .tasklet((contribution, chunkContext) -> {
                     System.out.println("step1 executed!!s");
@@ -23,9 +23,36 @@ public class BatchConfiguration {
     }
 
     @Bean
-    public Job firstJob(JobRepository jobRepository, Step firstStep) {
+    public Step step2(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+        return new StepBuilder("step2", jobRepository)
+                .tasklet((contribution, chunkContext) -> {
+                    boolean isError = false;
+                    if (isError) {
+                        throw new RuntimeException("step2 failed!!");
+                    }
+                    System.out.println("step2 executed!!s");
+                    return RepeatStatus.FINISHED;
+                }, transactionManager)
+                .build();
+    }
+
+    @Bean
+    public Step step3(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+        return new StepBuilder("step3", jobRepository)
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("step3 executed!!s");
+                    return RepeatStatus.FINISHED;
+                }, transactionManager)
+                .build();
+    }
+
+    @Bean
+    public Job firstJob(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
         return new JobBuilder("job1", jobRepository)
-                .start(firstStep)
+                .preventRestart()
+                .start(step1(jobRepository, transactionManager))
+                .next(step2(jobRepository, transactionManager))
+                .next(step3(jobRepository, transactionManager))
                 .build();
     }
 }
